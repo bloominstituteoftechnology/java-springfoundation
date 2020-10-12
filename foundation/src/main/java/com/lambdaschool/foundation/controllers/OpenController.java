09b5abc2331f67bdbde5890ad569c496e8460423
John Mitchell <jrmmba@outlook.com>
2020-10-12 13:26:49 -0700

    Updating Documentation

--- foundation/src/main/java/com/lambdaschool/foundation/controllers/OpenController.java
+++ foundation/src/main/java/com/lambdaschool/foundation/controllers/OpenController.java
@@ -7,6 +7,8 @@ import com.lambdaschool.foundation.services.RoleService;
 import com.lambdaschool.foundation.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -124,6 +126,7 @@ public class OpenController
             HttpStatus.CREATED);
     }
 
+
     /**
      * Prevents no favicon.ico warning from appearing in the logs. @ApiIgnore tells Swagger to ignore documenting this as an endpoint.
      */
@@ -133,5 +136,4 @@ public class OpenController
     {
 
     }
-
 }
